ac36f022b6712b4fe211f20219bb304c57ceb30f
Gabriel Silva Lima <gabriel.osilvalima@gmail.com>
2021-04-07 12:43:26 -0300

    FirstCommit

---
+++ src/test/java/br/com/gabriel/www/BuscarCep/BuscarCepApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gabriel.www.BuscarCep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuscarCepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
